5ad78a687f90e3fe918337b58fd491c22be10093
Sanja <sanja.developer@gmail.com>
2020-07-15 12:27:15 +0200

    Add Moshi and AppModule

---
+++ app/src/main/java/com/example/currencyconverter/di/ApiModule.kt
@@ -0,0 +1,14 @@
+package com.example.currencyconverter.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi() : Moshi = Moshi.Builder().build()
+}
