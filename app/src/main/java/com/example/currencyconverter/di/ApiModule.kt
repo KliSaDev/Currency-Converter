c8bd8204a1b0a3c20d357c692b153c7c2f864077
Sanja <sanja.developer@gmail.com>
2020-07-16 09:43:13 +0200

    Provide OkHttpClient in ApiModule

--- app/src/main/java/com/example/currencyconverter/di/ApiModule.kt
+++ app/src/main/java/com/example/currencyconverter/di/ApiModule.kt
@@ -1,8 +1,12 @@
 package com.example.currencyconverter.di
 
+import com.example.currencyconverter.BuildConfig
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
@@ -11,4 +15,19 @@ class ApiModule {
     @Provides
     @Singleton
     fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+
+        val okHttpClient = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG) {
+            okHttpClient.addInterceptor(HttpLoggingInterceptor(HttpLoggingInterceptor.Logger {
+                Timber.tag("OkHttp").i(it)
+            }).apply { level = HttpLoggingInterceptor.Level.BODY })
+        }
+
+        return okHttpClient.build()
+    }
 }
