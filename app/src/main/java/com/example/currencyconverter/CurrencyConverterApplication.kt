4644346a63f2152a88fa6151662a324530c512b5
Sanja <sanja.developer@gmail.com>
2020-07-16 09:27:43 +0200

    Add Timber and initialize it in CurrencyConverterApplication class

--- app/src/main/java/com/example/currencyconverter/CurrencyConverterApplication.kt
+++ app/src/main/java/com/example/currencyconverter/CurrencyConverterApplication.kt
@@ -3,10 +3,23 @@ package com.example.currencyconverter
 import com.example.currencyconverter.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
 
 class CurrencyConverterApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().context(this).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
