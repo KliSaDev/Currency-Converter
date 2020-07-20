3da1e4cc2ea7758e74eb6a06db013f4ee6317ccd
Sanja <sanja.developer@gmail.com>
2020-07-20 11:08:38 +0200

    Add MainActivity onCreate method and add it as a launcher Activity in Manifest

--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.currencyconverter.ui
 
+import android.os.Bundle
 import com.example.currencyconverter.BaseActivity
+import com.example.currencyconverter.R
+import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Timber.d("onCreate, Main")
+    }
 }
