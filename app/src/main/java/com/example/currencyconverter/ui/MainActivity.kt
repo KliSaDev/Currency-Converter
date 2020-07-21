a191c30c05a8760a7eff1dbdfa2737f225e86f8e
Sanja <sanja.developer@gmail.com>
2020-07-21 10:03:55 +0200

    Add kotlinOptions in build.gradle to support extension function viewModels

--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.currencyconverter.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.currencyconverter.BaseActivity
 import com.example.currencyconverter.R
 
 class MainActivity : BaseActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.init()
     }
 }
