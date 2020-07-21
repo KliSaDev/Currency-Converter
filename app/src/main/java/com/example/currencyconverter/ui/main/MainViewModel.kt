ead5860596076f74b994372424cac15b8bb5acf7
Sanja <sanja.developer@gmail.com>
2020-07-21 12:11:11 +0200

    Add annotations to bind method in MainViewModelModule

--- app/src/main/java/com/example/currencyconverter/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/currencyconverter/ui/main/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.currencyconverter.ui.main
 
 import com.example.currencyconverter.BaseViewModel
+import com.example.currencyconverter.di.annotations.ViewModelKey
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 import timber.log.Timber
 
 class MainViewModel : BaseViewModel<MainState, MainEvent>() {
@@ -16,5 +18,7 @@ class MainViewModel : BaseViewModel<MainState, MainEvent>() {
 abstract class MainViewModelModule {
 
     @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
     abstract fun bindMainViewModel(viewModel: MainViewModel) : MainViewModel
 }
