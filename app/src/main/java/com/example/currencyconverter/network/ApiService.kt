3d4cef6bc36a78f239bc0239e3b10d32db381f8c
Sanja <sanja.developer@gmail.com>
2020-08-01 19:37:02 +0200

    Add method in ApiService for getting all currencies

--- app/src/main/java/com/example/currencyconverter/network/ApiService.kt
+++ app/src/main/java/com/example/currencyconverter/network/ApiService.kt
@@ -1,4 +1,11 @@
 package com.example.currencyconverter.network
 
+import com.example.currencyconverter.data.Currency
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET
+    fun getAllCurrencies(): Single<List<Currency>>
 }
