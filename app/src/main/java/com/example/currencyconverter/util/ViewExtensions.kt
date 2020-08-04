33b7f952e3a30922956d786afb2f0906e310caa5
Sanja <sanja.developer@gmail.com>
2020-08-04 13:40:29 +0200

    Add view extensions for showing and hiding views

--- app/src/main/java/com/example/currencyconverter/util/ViewExtensions.kt
+++ app/src/main/java/com/example/currencyconverter/util/ViewExtensions.kt
@@ -5,5 +5,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
 fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View =
     LayoutInflater.from(context).inflate(layout, this, attachToRoot)
