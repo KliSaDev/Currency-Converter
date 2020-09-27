05eb7522695c75972afacda712e6a9110219788c
Sanja <sanja.developer@gmail.com>
2020-09-27 21:10:33 +0200

    Rename selectedFromCurrencyButton and setup it in ConvertCurrencyFragment

--- app/src/main/java/com/example/currencyconverter/ui/main/convert/ConvertCurrencyFragment.kt
+++ app/src/main/java/com/example/currencyconverter/ui/main/convert/ConvertCurrencyFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.currencyconverter.BaseFragment
 import com.example.currencyconverter.R
+import com.example.currencyconverter.data.Currency
 import com.example.currencyconverter.util.toast
 import kotlinx.android.synthetic.main.fragment_convert_currency.*
 
@@ -40,12 +41,25 @@ class ConvertCurrencyFragment : BaseFragment() {
     }
 
     private fun setupLayout(state: ConvertCurrencyState) {
+        setupSelectedFromCurrencyButton(state.selectedFromCurrency)
         fromCurrencyInput.apply {
             setText(state.fromValue)
             fromCurrencyInput.text?.length?.let { setSelection(it) }
         }
         toCurrencyInput.setText(state.toValue)
     }
+
+    private fun setupSelectedFromCurrencyButton(selectedFromCurrency: Currency) {
+        selectedFromCurrencyButton.apply {
+            text = selectedFromCurrency.currencyName
+            setOnClickListener { showChangeCurrencyAlertDialog() }
+        }
+    }
+
+    private fun showChangeCurrencyAlertDialog() {
+        // TODO: implement
+    }
+
 
     private fun setupCalculateButton() {
         buttonCalculate.setOnClickListener {
