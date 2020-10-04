62549411231d899bbe2840d54f2d3a0f548605f0
Sanja <sanja.developer@gmail.com>
2020-10-04 13:42:14 +0200

    Remove unused function from BaseActivity

--- app/src/main/java/com/example/currencyconverter/BaseActivity.kt
+++ app/src/main/java/com/example/currencyconverter/BaseActivity.kt
@@ -1,7 +1,5 @@
 package com.example.currencyconverter
 
-import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
 import com.example.currencyconverter.di.ViewModelProviderFactory
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -10,9 +8,4 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var factory: ViewModelProviderFactory
-
-    inline fun <reified T : ViewModel> getViewModel(viewModel: T): T {
-        val model by viewModels<T>() {factory}
-        return model
-    }
 }
