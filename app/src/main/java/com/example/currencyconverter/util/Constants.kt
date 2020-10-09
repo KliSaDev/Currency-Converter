391f3bb681eb2446860cd0bc7580b4c4eac8450e
Sanja <sanja.developer@gmail.com>
2020-10-09 16:42:33 +0200

    Change values for chart line and circle radius

--- app/src/main/java/com/example/currencyconverter/util/Constants.kt
+++ app/src/main/java/com/example/currencyconverter/util/Constants.kt
@@ -10,12 +10,12 @@ const val MAX_DAILY_VALUES = 7
 const val DAYS_TO_SUBTRACT_FOR_WEEKLY_RATES = 6L
 
 // WEEKLY RATES CHART
-const val MIN_AND_MAX_VALUE_OFFSET_FOR_RATE = 0.5f
+const val MIN_AND_MAX_VALUE_OFFSET_FOR_RATE = 0.3f
 const val BORDER_WIDTH = 3.0f
 
 const val NUMBER_OF_DIGITS_TO_ROUND = 3
-const val LINE_WIDTH = 3f
-const val CIRCLE_RADIUS = 5f
+const val LINE_WIDTH = 2f
+const val CIRCLE_RADIUS = 4f
 
 const val X_AXIS_GRANULARITY = 1f
 const val X_AXIS_OFFSET_FOR_VALUES = 0.3f
